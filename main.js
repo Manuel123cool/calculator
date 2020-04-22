e0860109c68c1f76763eba45ad766d9d23e387c7
Manuel Kümpel <46404467+Manuel123cool@users.noreply.github.com>
2020-04-22 16:58:09 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,97 @@
+"use strict";
+
+let c = document.getElementById("calculator");
+let ctx = c.getContext("2d");
+
+function drawRectBorder(xPos, yPos, width, height) {
+    ctx.moveTo(xPos, yPos);
+    ctx.lineTo(xPos + width, yPos);
+    
+    ctx.moveTo(xPos, yPos);
+    ctx.lineTo(xPos, yPos + height);
+
+    ctx.moveTo(xPos, yPos + height);
+    ctx.lineTo(xPos + width, yPos + height);
+
+    ctx.moveTo(xPos + width, yPos);
+    ctx.lineTo(xPos + width, yPos + height);
+}
+
+function drawText(text) {
+    ctx.clearRect(6, 6, 389, 59);
+    ctx.font = "30px Arial";
+    ctx.fillText(text, 10, 45);
+}
+
+let firstDigits = "";
+let secondDigits = "";
+let operator = "";
+let enterSecondTime = false;
+
+function calculator(text) {
+    console.log(text);
+    if (text.substring(0, 5) == "Digit" && operator != "") {
+        firstDigits += text.charAt(5); 
+        return;
+    }
+            
+    if (text.substring(0, 5) == "Digit") {
+        secondDigits += text.charAt(5);
+    } 
+     
+    switch (text) {
+        case "NumpadDivide":
+            operator = "/";
+            break;
+        case "NumpadMultiply":
+            operator = "*";
+            break;
+        case "NumpadAdd":
+            operator = "+";
+            break;
+        case "NumpadSubtract":
+            operator = "-";
+            break;
+    }
+    if (text == "Enter") {
+        if (enterSecondTime) {
+            secondDigits = "";
+            operator = "";
+            firstDigits = "";
+            drawText("0");
+            enterSecondTime = false;
+        }
+        switch (operator) {
+            case "*":
+                drawText(firstDigits * secondDigits);
+                enterSecondTime = true;
+                break;
+            case "/":
+                drawText(firstDigits / secondDigits);
+                enterSecondTime = true;
+                break;
+            case "+":
+                drawText(firstDigits + secondDigits);
+                enterSecondTime = true;
+                break;
+            case "-":
+                drawText(secondDigits - firstDigits);
+                enterSecondTime = true;
+                break;
+        }
+    }
+}
+
+function calcText(text) {
+    calculator(text);
+}
+
+function keyPressEvent(e) {
+    calcText(e.code, false);
+}
+
+drawRectBorder(5, 5, 390, 60);
+
+window.addEventListener("keypress", keyPressEvent);
+
+ctx.stroke();
